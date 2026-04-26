9bab98f7c616cf2a8377bc24efb93d4341718121
DEEPAK-V2007 <deepuvenk25@gamil.com>
2026-04-26 16:18:58 +0530

    Added the requirement

--- app.py
+++ app.py
@@ -149,4 +149,5 @@ def humanize_text3(text):
     return response.choices[0].message.content
 
 if(__name__=="__main__"):
-    app.run(debug=False, host='0.0.0.0', port= 5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
